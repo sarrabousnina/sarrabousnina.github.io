3980693b623696de1660b8354d271b610d01663b
sarra.bousnina@esprit.tn <sarra.bousnina@esprit.tn>
2025-09-15 17:49:21 +0100

    renamed repo

--- lib/asset.ts
+++ lib/asset.ts
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
-const repo = "Sarra_Bousnina_Portfolio"; // 👈 your repo name
+const repo = "sarrabousnina.github.io"; // 👈 your repo name
 
 export function getAssetPath(path: string) {
   return isProd ? `/${repo}${path}` : path;
